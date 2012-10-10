fa8576af735e0b892472492ef520e79d3c5b7f5a
Michael Neale <michael.neale@gmail.com>
2012-10-10 16:17:55 +0200

    added notes on next steps after deploying

--- src/mywebapp/views.clj
+++ src/mywebapp/views.clj
@@ -31,15 +31,37 @@
     [:p "Welcome to the Clojure Clickstart!"]
     [:p
      [:a {:class "btn btn-primary btn-large"
-          :href "http://wiki.cloudbees.com/bin/view/Main/"}
-      "Get help with Cloudbees."]]]))
+          :href "http://developer.cloudbees.com/bin/view/Main/"}
+      "Get help with Cloudbees."]]]
+      
+      [:div {:class "well"}
+        [:p
+          [:h3 "Next steps"]
+          [:ul
+            [:li (str "Make sure you have an ssh public key setup on" 
+                      "<a href=\"https://grandcentral.cloudbees.com/user/ssh_keys\">
+                      Cloudbees</a> and then run:")]
+            [:li 
+              [:code (str 
+                      "git clone ssh://git@git.cloudbees.com/"
+                      "<script>document.write"
+                        "(location.hostname.split(\".\")[1])</script>"
+                      "/<script>document.write"
+                      "(location.hostname.split(\".\")[0])</script>.git")]]
+            [:li "Make your changes and then push to the git repo"]  
+            [:li "This will trigger a build and deploy cycle on cloudbees"]]]]))
 
 (defn about-page []
   (template
    [:div {:class "well"}
-    [:h1 "About"]
+    [:h1 "About This:"]
     [:p "This Clojure clickstart was developed by members of the "
      [:a {:href "http://www.meetup.com/Austin-Clojure-Meetup/"} "Austin Clojure Meetup"]
      ".  You can find us as "
      [:a {:href "https://github.com/AustinClojure"} "AustinClojure on github"]
-     "."]]))
+     "."]
+     [:h1 "About ClickStarts:"]
+      "Read about what ClickStarts are "
+     [:a {:href "https://developer.cloudbees.com/bin/view/RUN/ClickStart"} "at CloudBees"]
+
+     ]))
