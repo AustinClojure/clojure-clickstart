81a0eafc7d22809827632d390e7293c6d0298919
Norman Richards <orb@nostacktrace.com>
2012-10-21 17:53:05 -0500

    add repl page

--- src/mywebapp/routes.clj
+++ src/mywebapp/routes.clj
@@ -1,27 +1,29 @@
 (ns mywebapp.routes
   (:use compojure.core
-        mywebapp.views)
+        mywebapp.views
+        mywebapp.repl)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [cemerick.drawbridge]))
 
 (defroutes app-routes
   (GET "/" [] (index-page))
+  (GET "/repl" [] (repl-page))
   (GET "/about" [] (about-page))
   (route/resources "/"))
 
 
-(if-let [secret (System/getProperty "replsecret")]
-  (defroutes repl-routes
-    (ANY "/repl/:guess" [guess :as request] 
-         (if (= secret guess)
-           ((cemerick.drawbridge/ring-handler) request)
-           (route/not-found "secret does not match replsecret"))))
-  
-  (defroutes repl-routes
-    (ANY "/repl/:guess" [] 
-         (route/not-found "replsecret not set. use bees config:set {account/appname} replsecret={SECRET}"))))
+(defn repl-handler [request guess]
+  (if (secret-set?)
+    (if (test-secret guess)
+      ((cemerick.drawbridge/ring-handler) request)
+      (route/not-found "secret does not match replsecret"))
+    
+    (route/not-found "replsecret not set. use bees config:set {account/appname} replsecret={SECRET}")))
 
+(defroutes repl-routes
+  (ANY "/repl/:guess" [guess :as request] 
+       (repl-handler request guess)))
 
 (defroutes all-routes
   app-routes
