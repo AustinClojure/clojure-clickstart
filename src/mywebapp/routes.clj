36c6a681d722402a8ed38153ddeba2972c894710
Norman Richards <orb@nostacktrace.com>
2012-10-21 16:47:34 -0500

    readme and config change

--- src/mywebapp/routes.clj
+++ src/mywebapp/routes.clj
@@ -11,16 +11,16 @@
   (route/resources "/"))
 
 
-(if-let [secret (System/getProperty "repl-secret")]
+(if-let [secret (System/getProperty "replsecret")]
   (defroutes repl-routes
     (ANY "/repl/:guess" [guess :as request] 
          (if (= secret guess)
            ((cemerick.drawbridge/ring-handler) request)
-           (route/not-found "secret does not match repl-secret"))))
-
+           (route/not-found "secret does not match replsecret"))))
+  
   (defroutes repl-routes
     (ANY "/repl/:guess" [] 
-         (route/not-found "repl-secret not set. use bees config:set {account/appname} repl-secret={SECRET}"))))
+         (route/not-found "replsecret not set. use bees config:set {account/appname} replsecret={SECRET}"))))
 
 
 (defroutes all-routes
