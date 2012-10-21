da7b3db4dd51e2d451efe5440b69992341bd67aa
Norman Richards <orb@nostacktrace.com>
2012-10-21 16:37:52 -0500

    use repl-secret config

--- src/mywebapp/routes.clj
+++ src/mywebapp/routes.clj
@@ -8,17 +8,25 @@
 (defroutes app-routes
   (GET "/" [] (index-page))
   (GET "/about" [] (about-page))
-  (route/resources "/")  
+  (route/resources "/"))
 
-  (if-let [secret (System/getProperty "magicword")]
-    
-    (let [nrepl-handler (cemerick.drawbridge/ring-handler)]
-      (ANY (str "/repl/" secret) request (nrepl-handler request)))
-    
-    (route/not-found "No page"))
-  
+
+(if-let [secret (System/getProperty "repl-secret")]
+  (defroutes repl-routes
+    (ANY "/repl/:guess" [guess :as request] 
+         (if (= secret guess)
+           ((cemerick.drawbridge/ring-handler) request)
+           (route/not-found "secret does not match repl-secret"))))
+
+  (defroutes repl-routes
+    (ANY "/repl/:guess" [] 
+         (route/not-found "repl-secret not set. use bees config:set {account/appname} repl-secret={SECRET}"))))
+
+
+(defroutes all-routes
+  app-routes
+  repl-routes
   (route/not-found "No page"))
 
-
 (def app
-  (handler/site app-routes))
+  (handler/site all-routes))
