577ceb69c71bea970c8ce7a16b5ca02a5e8a99dd
Norman Richards <orb@nostacktrace.com>
2012-10-21 13:47:01 -0500

    drawbridge1

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
-                 [hiccup "1.0.1"]]
+                 [hiccup "1.0.1"]
+                 [org.clojure/tools.nrepl "0.2.0-beta10"]
+                 [com.cemerick/drawbridge "0.0.6"]]
   :plugins [[lein-ring "0.7.3"]]
   :ring {:handler mywebapp.routes/app}
   :profiles
